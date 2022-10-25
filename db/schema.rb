fa0d133d80e2ba09ebbd1ced6d46707476b6c98e
Jesus Barboza <jbarz20@gmail.com>
2022-10-25 16:22:39 -0500

    create Type model and a join table between Type and Pokemon.

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_212006) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "weight"
+    t.integer "height"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pokemons_types", id: false, force: :cascade do |t|
+    t.bigint "pokemon_id", null: false
+    t.bigint "type_id", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.date "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'httparty'
+require 'json'
+
+# This class is just for seed the db.
+Pokemon.destroy_all
+# Type.destroy_all
+def pokemons_info(url)
+  response = HTTParty.get(url)
+  raise ResponseError, response unless response.success?
+
+  JSON.parse(response.body, symbolize_names: true)
+end
+
+url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+pokemons = pokemons_info(url)
+
+pokemons[:results].each do |pokemon|
+  url_pokemon = pokemons_info(pokemon[:url])
+  image = URI.open(url_pokemon[:sprites][:front_default])
+  pokemon = Pokemon.create(name: pokemon[:name], weight: url_pokemon[:weight], height: url_pokemon[:height], image: url_pokemon[:sprites][:front_default])
+  # pokemon.cover.attach(io: image, filename: 'name.png', content_type: 'image/png')
+end
+
+url_type = 'https://pokeapi.co/api/v2/type'
+types = pokemons_info(url_type)
